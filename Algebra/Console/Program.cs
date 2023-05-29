ec42bfee698e79ade01a036395496acf203c5cf3
AntoninoRando <75914073+AntoninoRando@users.noreply.github.com>
2023-05-30 00:15:22 +0200

    Better Input

--- Algebra/Console/Program.cs
+++ Algebra/Console/Program.cs
@@ -12,7 +12,7 @@ string input = take_congruences();
 
 if (parse_input(input) == 1) // Error occurred.
 {
-    print_help();
+    printHelp();
 }
 
 /* FUNCTIONS */
@@ -20,80 +20,100 @@ if (parse_input(input) == 1) // Error occurred.
 string take_congruences()
 {
     string input = string.Empty;
-    string line = string.Empty;
-    ConsoleKeyInfo keyInfo;
-    int enter_pressed = 0, congruences = 0;
+    int congruences = 0, systems = 0;
 
-    Console.Write($"\n({congruences + 1})\t\x1B[36m"); // Setting cyan color for user input.
+    Console.WriteLine("\x1B[36m"); // Set cyan color for user input.
     while (true)
     {
-        keyInfo = Console.ReadKey();
 
-        if (keyInfo.Key == ConsoleKey.Enter)
+        if (systems < 1)
         {
-            if (enter_pressed == 1 || line == string.Empty)
-            {
-                Console.Write(col("   \tEXIT", "0"));
-                break;
-            }
+            Console.Write($"\x1B[0m({congruences + 1})");
+        }
+        Console.Write("\t\x1B[36m");
 
-            congruences++;
-            Console.Write($"\n\x1B[0m({congruences + 1})\t\x1B[36m");
-            line = String.Empty;
-            input += '\n';
-            enter_pressed = 1;
+        string? line = Console.ReadLine();
+
+        if (String.IsNullOrEmpty(line) || String.IsNullOrWhiteSpace(line))
+        {
+            Console.Write(col("EXIT\n", "0"));
+            break;
+        }
+
+        if (line == "--help")
+        {
+            Console.Write("\x1B[0m\n");
+            return "--help";
+        }
+
+        if (line[^1] == ';')
+        {
+            systems++;
         }
         else
         {
-            input += keyInfo.KeyChar;
-            line += keyInfo.KeyChar;
-            enter_pressed = 0;
+            congruences -= systems; // Keep advancing the counter without considering all the equations now typed.
+            systems = 0;
         }
+
+        input += line + '\n';
+        congruences++;
+
     }
-    Console.WriteLine("\x1B[0m"); // Reset color and put a new line.
+    Console.Write("\x1B[0m\n"); // Reset color and put a new line.
 
     return input;
 }
 
-void print_help()
+void printHelp()
 {
-    Console.WriteLine("\nUSAGE GUIDE");
+    Console.Write("\n");
+    Console.WriteLine("USAGE GUIDE");
     Console.WriteLine("----- Linear Congruence -----");
     Console.WriteLine("-\tYou can write numbers in decimal (default), binary (0b) or hexadecimal (0x): 123, 0xA19, 0b-111000");
     Console.WriteLine("-\tUnknowns can be any symbols other than numbers and special operators;");
-    Console.WriteLine("-\tMathematical operators are: +, -, /, *, =, mod (X);. You can remap them, except for mod( X), writing: `remap old:new`");
+    Console.WriteLine("-\tMathematical operators are: +, -, /, *, =, mod X;. You can remap them, except for mod X, writing: `remap old:new`");
     Console.WriteLine("-\tYou can write as many white-spaces you whish.");
     Console.Write("\n");
     Console.WriteLine("----- System of Congruence -----");
-    Console.WriteLine("-\tSimply type a new congruence after the end of mod (X);.");
-    Console.WriteLine("-\tYou can use ENTER to start a new line and better organize your work.");
+    Console.WriteLine("-\tSeparate each congruence by a semicolon ;.");
+}
+
+int readModule(string congruence, ref int modIndex)
+{
+    int mod = 0;
+
+    for (int j = 1; j <= congruence.Length; j++)
+    {
+        if (!Char.IsDigit(congruence[^j]))
+        {
+            modIndex = j;
+            break;
+        }
+
+        mod += (congruence[^j] - '0') * (int)Math.Pow(10, j - 1);
+    }
+
+    return mod;
 }
 
 int parse_input(string input)
 {
     if (input == "--help")
     {
-        print_help();
+        printHelp();
         return 0;
     }
 
-    char[] operators = { '+', '-' };
-    string[] congruences = input.Split('\n');
+    string operators = "+-";
+    string[] congruences = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
     for (int i = 0; i < congruences.Length; i++)
     {
         string cong = congruences[i];
         string trim = Regex.Replace(cong, @"\s+", ""); // Remove all whitespaces.
 
-        int mod = 0, j;
-        for (j = 2; j <= trim.Length; j++) // At the end ^1 is ) then ^2 is a digit.
-        {
-            if (!Char.IsDigit(trim[^j]))
-            {
-                break;
-            }
-
-            mod += (trim[^j] - '0') * (int)Math.Pow(10, j - 2);
-        }
+        int j = 0;
+        int mod = readModule(trim, ref j);
 
         if (mod == 0)
         {
@@ -101,7 +121,7 @@ int parse_input(string input)
             return 1;
         }
 
-        trim = trim[^trim.Length..^(j + 3)]; // Removing mod(X).;
+        trim = trim[^trim.Length..^(j + 1)]; // Removing modX.;
 
         string[] operands = trim.Split('=');
 
@@ -179,10 +199,16 @@ int parse_input(string input)
 
         }
 
-        Console.Write($"The solution of (1) {lOperands[0]}x = {rOperands[0]} (mod {mod}) is ");
-        Console.WriteLine(linear_resolution(lOperands[0], rOperands[0], mod));
+        int? sol = linear_resolution(lOperands[0], rOperands[0], mod);
 
-
+        if (sol != null)
+        {
+            Console.Write($"One solution of \x1B[36m({i + 1}) {lOperands[0]}x = {rOperands[0]} (mod {mod})\x1B[0m is ");
+            Console.WriteLine($"\x1B[32;1m{sol}\x1B[0m.");
+        }
+        else {
+            Console.Write($"The \x1B[36m({i + 1}) {lOperands[0]}x = {rOperands[0]} (mod {mod})\x1B[0m has \x1B[32;1mNO solutions\x1B[0m.");
+        }
     }
     return 0;
 }
@@ -193,11 +219,11 @@ string col(string text, string color)
 }
 
 // Resolve ax=b mod(n)
-int linear_resolution(int ax, int b, int n, int printProcess = 0)
+int? linear_resolution(int ax, int b, int n, int printProcess = 0)
 {
     if (b % gcd(ax, n, printProcess) != 0) // Check (a,n) | b
     {
-        return -1;
+        return null;
     }
 
     b = b % n;
@@ -247,9 +273,9 @@ int gcd(int a, int b, int printProcess = 0)
             int d = (int)a / b;
             r = a % b;
 
-            Console.Write($"{a} = \x1B[1m{r2}\x1B[0m•{d} + \x1B[1m{r}\x1B[0m");
+            Console.Write($"{a} = \x1B[1m{b}\x1B[0m•{d} + \x1B[1m{r}\x1B[0m");
 
-            a = d;
+            a = r2;
             b = r;
         }
         while (r != 0);
@@ -279,7 +305,7 @@ int gcd(int a, int b, int printProcess = 0)
         int d = (int)a / b;
         r = a % b;
 
-        a = d;
+        a = r2;
         b = r;
     }
     while (r != 0);
