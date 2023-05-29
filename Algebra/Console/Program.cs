38ad479ee870db8bcf3940aed3c9b3d3da28e4d3
AntoninoRando <75914073+AntoninoRando@users.noreply.github.com>
2023-05-29 19:50:59 +0200

    Supports linear congruences as input

--- Algebra/Console/Program.cs
+++ Algebra/Console/Program.cs
@@ -15,6 +15,8 @@ if (parse_input(input) == 1) // Error occurred.
     print_help();
 }
 
+/* FUNCTIONS */
+
 string take_congruences()
 {
     string input = string.Empty;
@@ -38,6 +40,7 @@ string take_congruences()
             congruences++;
             Console.Write($"\n\x1B[0m({congruences + 1})\t\x1B[36m");
             line = String.Empty;
+            input += '\n';
             enter_pressed = 1;
         }
         else
@@ -74,51 +77,113 @@ int parse_input(string input)
         return 0;
     }
 
-    string[] congruences = input.Split(";");
-    foreach (string cong in congruences)
+    char[] operators = { '+', '-' };
+    string[] congruences = input.Split('\n');
+    for (int i = 0; i < congruences.Length; i++)
     {
+        string cong = congruences[i];
         string trim = Regex.Replace(cong, @"\s+", ""); // Remove all whitespaces.
 
-        int mod = 0, i;
-        for (i = 2; i <= trim.Length; i++) // At the end ^1 is ) then ^2 is a digit.
+        int mod = 0, j;
+        for (j = 2; j <= trim.Length; j++) // At the end ^1 is ) then ^2 is a digit.
         {
-            if (!Char.IsDigit(trim[^i]))
+            if (!Char.IsDigit(trim[^j]))
             {
                 break;
             }
 
-            mod += (trim[^i] - '0') * (int)Math.Pow(10, i - 2);
+            mod += (trim[^j] - '0') * (int)Math.Pow(10, j - 2);
         }
 
         if (mod == 0)
         {
-            Console.WriteLine(col("error", "41") + col(": Invalid module.", "31"));
+            Console.WriteLine(col($"error at ({i + 1})", "41") + col(": Invalid module.", "31"));
             return 1;
         }
 
-        trim = trim[^trim.Length..^(i + 3)]; // Removing mod(X).;
+        trim = trim[^trim.Length..^(j + 3)]; // Removing mod(X).;
 
-        string[] parts = trim.Split('=');
+        string[] operands = trim.Split('=');
 
-        if (parts.Length > 2)
+        if (operands.Length > 2)
         {
-            Console.WriteLine(col("error", "41") + col(": Too many equals.", "31"));
+            Console.WriteLine(col($"error at ({i + 1})", "41") + col(": Too many equals.", "31"));
             return 1;
         }
-        else if (parts.Length < 2)
+        else if (operands.Length < 2)
         {
-            Console.WriteLine(col("error", "41") + col(": Missing equal.", "31"));
+            Console.WriteLine(col($"error at ({i + 1})", "41") + col(": Missing equal.", "31"));
             return 1;
         }
 
-        string[] lOperands = parts[0].Split('+', '-', '*');
-        string[] rOperands = parts[1].Split('+', '-', '*');
+        List<int> lOperands = new List<int>();
+        j = 0;
+        int digit = 0;
+        foreach (char c in operands[0])
+        {
+            if (lOperands.Count < j + 3)
+            {
+                lOperands.Add(0);
+                lOperands.Add('\0');
+                lOperands.Add('\0');
+            }
 
-        int ax = 3522, b = 1818;
-        int sol = linear_resolution(ax, b, mod, 1);
-        Console.WriteLine($"The solution to {ax}x = {b} (mod {mod}) IS {sol}.");
+            // This operand ended.
+            if (operators.Contains(c))
+            {
+                lOperands[j + 1] = c;
+                j++;
+                digit = 0;
+                // Revert number lOperands[j]
+            }
+            else if (Char.IsDigit(c))
+            {
+                lOperands[j] += (c - '0') * (int)Math.Pow(10, digit);
+                digit++;
+            }
+            else
+            {
+                lOperands[j + 2] = c;
+            }
+
+        }
+        List<int> rOperands = new List<int>();
+        j = 0;
+        digit = 0;
+        foreach (char c in operands[1])
+        {
+            if (rOperands.Count < j + 3)
+            {
+                rOperands.Add(0);
+                rOperands.Add('\0');
+                rOperands.Add('\0');
+            }
+
+            // This operand ended.
+            if (operators.Contains(c))
+            {
+                rOperands[j + 1] = c;
+                j++;
+                digit = 0;
+                // Revert number rOperands[j]
+            }
+            else if (Char.IsDigit(c))
+            {
+                rOperands[j] += (c - '0') * (int)Math.Pow(10, digit);
+                digit++;
+            }
+            else
+            {
+                rOperands[j + 2] = c;
+            }
+
+        }
+
+        Console.Write($"The solution of (1) {lOperands[0]}x = {rOperands[0]} (mod {mod}) is ");
+        Console.WriteLine(linear_resolution(lOperands[0], rOperands[0], mod));
+
+
     }
-
     return 0;
 }
 
